e997e38fce42f80bf815beba3f10da868a05168d
Nicoller-24 <166450090+Nicoller-24@users.noreply.github.com>
2024-10-01 17:51:57 +0000

    Api finalizada

---
+++ src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if 0 <= position < len(todos):
+        del todos[position]
+        return jsonify(todos)
+    else:
+        return jsonify({"error": "Position out of range"}), 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
